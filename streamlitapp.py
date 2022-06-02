c4e424a5ba78e244769cbc5bb1db74ee421358fa
DataSnowflake <106661263+DataSnowflake@users.noreply.github.com>
2022-06-02 11:58:02 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,8 +1,9 @@
-
 import streamlit
 streamlit.titke('my parents new heathly diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('fevorite menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
